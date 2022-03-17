dcfb7adedeecd4badfcdde1e8b1a4af1e3633990
Elias Kuiter <info@elias-kuiter.de>
2022-03-17 12:19:45 +0100

    Updated script

--- export.sh
+++ export.sh
@@ -2,15 +2,6 @@
 set -e
 CONTAINERS=(kconfigreader kclause stage2)
 
-rm -rf _export
-mkdir -p _export
-
 for container in ${CONTAINERS[@]}; do
-    docker save $container | gzip > _export/$container.tar.gz
+    docker save $container | gzip > $container.tar.gz
 done
-
-cp import.sh _export/
-cp -r _dimacs _export/dimacs
-cp -r _models _export/models
-cp -r _transform _export/transform
-cp -r _results.csv _export/results.csv

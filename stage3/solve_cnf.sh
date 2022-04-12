5211f97c6d88303a50b9c69809afa29cfee11cea
Elias Kuiter <info@elias-kuiter.de>
2022-04-12 11:12:09 +0200

    Calculate the same core/dead features for all iterations

--- stage3/solve_cnf.sh
+++ stage3/solve_cnf.sh
@@ -13,17 +13,17 @@ run-solver() (
     if cat $log | grep -q "SATISFIABLE" || cat $log | grep -q "^s " || cat $log | grep -q " of solutions" || cat $log | grep -q "# solutions" || cat $log | grep -q " models"; then
         model_count=$(cat $log | sed -z 's/\n# solutions \n/SHARPSAT/g' | grep -oP "((?<=Counting...)\d+(?= models)|(?<=  Counting... )\d+(?= models)|(?<=c model count\.{12}: )\d+|(?<=^s )\d+|(?<=^s mc )\d+|(?<=#SAT \(full\):   		)\d+|(?<=SHARPSAT)\d+|(?<=Number of solutions\t\t\t)[.e+\-\d]+)" || true)
         model_count="${model_count:-NA}"
-        echo $dimacs,$solver,$analysis$i,$(echo "($end - $start) * 1000000000 / 1" | bc),$model_count >> $res
+        echo $dimacs,$solver,$analysis$suffix,$(echo "($end - $start) * 1000000000 / 1" | bc),$model_count >> $res
     else
         echo "WARNING: No solver output for $dimacs with solver $solver and analysis $analysis" | tee -a $err
-        echo $dimacs,$solver,$analysis$i,NA,NA >> $res
+        echo $dimacs,$solver,$analysis$suffix,NA,NA >> $res
     fi
 )
 
 run-void-analysis() (
     cat $dimacs_path | grep -E "^[^c]" > input.dimacs
     echo "  Void feature model / feature model cardinality"
-    i=""
+    suffix=""
     run-solver
 )
 
@@ -37,6 +37,8 @@ run-core-dead-analysis() (
         clauses=$((clauses + 1))
         sed -i "s/^\(p cnf [[:digit:]]\+ \)[[:digit:]]\+/\1$clauses/" input.dimacs
         echo "$2$fnum 0" >> input.dimacs
+        echo "  $1 $f"
+        suffix="$i-$f"
         run-solver
         i=$(($i+1))
     done
@@ -56,22 +58,23 @@ touch $err
 rm -rf data/dimacs/*.features
 for dimacs_path in data/dimacs/*.dimacs; do
     dimacs=$(basename $dimacs_path .dimacs | sed 's/,/_/')
-    base=$(echo $dimacs_path | rev | cut -d, -f2- | rev)
+    base_it=$(echo $dimacs_path | rev | cut -d, -f2- | rev)
+    base=$(echo $base_it | sed 's/\(,.*,\).*,/\1/g')
     echo "Reading features for $dimacs"
     if [ ! -f $base.features ]; then
         touch $base.features
-        features=$(cat $base,z3.dimacs | grep -E "^c [1-9]" | grep -v 'k!'| cut -d' ' -f3 | shuf --random-source=<(yes $RANDOM_SEED))
+        features=$(cat $base_it,z3.dimacs | grep -E "^c [1-9]" | grep -v 'k!'| cut -d' ' -f3 | shuf --random-source=<(yes $RANDOM_SEED))
         i=1
         found=0
         while [ $found -lt $NUM_FEATURES ] && [ $i -le $(echo "$features" | wc -l) ]; do
             feature=$(echo "$features" | tail -n+$i | head -1)
-            if ([ ! -f $base,featureide.dimacs ] || (cat $base,featureide.dimacs | grep -q $feature)) &&
-               ([ ! -f $base,kconfigreader.dimacs ] || (cat $base,kconfigreader.dimacs | grep -q $feature)) &&
-               ([ ! -f $base,z3.dimacs ] || (cat $base,z3.dimacs | grep -q $feature)); then
+            if ([ ! -f $base_it,featureide.dimacs ] || (cat $base_it,featureide.dimacs | grep -q $feature)) &&
+               ([ ! -f $base_it,kconfigreader.dimacs ] || (cat $base_it,kconfigreader.dimacs | grep -q $feature)) &&
+               ([ ! -f $base_it,z3.dimacs ] || (cat $base_it,z3.dimacs | grep -q $feature)); then
                 echo $feature >> $base.features
                 found=$(($found+1))
             else
-                echo "WARNING: Feature $feature not found in all DIMACS files for $base" | tee -a $err
+                echo "WARNING: Feature $feature not found in all DIMACS files for $base_it" | tee -a $err
             fi
             i=$(($i+1))
         done
@@ -79,7 +82,7 @@ for dimacs_path in data/dimacs/*.dimacs; do
 done
 for dimacs_path in data/dimacs/*.dimacs; do
     dimacs=$(basename $dimacs_path .dimacs | sed 's/,/_/')
-    base=$(echo $dimacs_path | rev | cut -d, -f2- | rev)
+    base=$(echo $dimacs_path | rev | cut -d, -f2- | rev | sed 's/\(,.*,\).*,/\1/g')
     echo "Solving $dimacs"
     for solver in $SOLVERS; do
         for analysis in $ANALYSES; do
